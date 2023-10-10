b1c6d537559b675d6e0461e4ff8040588ea595ee
watanabe-pikaru <140324600+watanabe-pikaru@users.noreply.github.com>
2023-10-11 00:09:24 +0900

    Add files via upload

--- motivation/post_edit.php
+++ motivation/post_edit.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />      <!-- 文字コードを指定しています。推奨されている文字コード「UTF-8」を指定しましょう。UTF-8以外だとブラウザに表示されたとき文字化けを起こす可能性があります。 -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />     <!-- IE(Internet Explorer)を最新バージョンで動かさせるためのコードです。 --> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />     <!-- 「width=”device-width」は使用端末の横幅にページの横の長さを合わせるという意味です。また、「initial-scale=”1.0″」は初めにページが非表示されるとき、画面の倍率が1倍(つまりズームなし)という意味です。 -->
-    <title>記事追加</title>                                     <!-- タイトルは検索結果と、ブラウザのタブの上部に表示されます。 -->
+    <title>新規登録</title>                                     <!-- タイトルは検索結果と、ブラウザのタブの上部に表示されます。 -->
     <link rel="stylesheet" href="./style.css">                  <!-- このコードにより、別に用意してあるCSSファイルを読み込むことができます。 -->
     <link rel="stylesheet" href="sanitize.css">                 <!-- ブラウザデフォルトのCSSの違いを統一して、見た目を整える -->
   </head>
@@ -14,7 +14,7 @@
 
         <div class="list">
       
-            <h1>登録画面</h1>
+            <h1>新規登録</h1>
 
             <?php   //データベース接続
           
@@ -31,7 +31,7 @@
                         $db = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_user, $db_user);     // // PDOインスタンスを生成
                         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // エラーモードの設定
                         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // エミュレーションを停止。
-                } catch (PDOException $e) {             
+                    } catch (PDOException $e) {             
                     exit("データベースの接続に失敗しました"); 
                 }
             ?>
@@ -43,16 +43,18 @@
                 // form の method 属性が「post」なので送信されたデータは、$_POST という連想配列（スーパーグローバル変数）に格納されます。送信ボタンの name 属性が「send」なのでスーパーグローバル変数 $_POST のキーに「send」を指定して $_POST['send'] とします。isset($_POST['send']) が true の場合、送信ボタンがクリックされてデータが送信され、 $_POST['send']が定義済みになっています。
                 if (isset($_POST['send'])) {
                         if ($_POST['post_content'] == ''){
-                            echo "名言記入欄が空白です。入力してください";
+                            echo "<div class=echo_red>";     //赤字
+                            echo "記入欄が空白です。入力してください";
+                            echo "</div>";
                         }
                     }
             ?>
 
             <?php     //データ保存
 
-                // 登録実行
+                // 登録実行   
                 // 登録ボタン押されて、名言が空欄で無かったなら、データベース保存開始
-                if (isset($_POST['send']) && $_POST['post_content'] != "") {
+                if (isset($_POST['send']) && $_POST['post_content'] != "" ) {
                     // データベースへ保存
                     // beginTransaction()でトランザクション開始
                     // INSERT文はテーブルにデータを登録します。テーブル名のあとにカッコで、列名を並べます。VALUESのあとのカッコに、登録したい値をカンマで区切って並べます。列名と値は順番を合わせる必要があります。
@@ -60,29 +62,47 @@
                     // executeメソッドでプレースホルダに値をセット。execute関数を使うことでSQL文を実行。
                     // 以上全てが問題無ければcommit()で実行
                     // 途中でエラーが出たらロールバックで中断
-                    try {
+
+                $db_host = "localhost";           // データベースのホスト名
+                $db_name = "motivation";          // データベースの名前
+                $db_user = "root";                // データベース接続ユーザー
+                $db_pass = "root";                // データベース接続パスワード
+
+                try {
+                        $db = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_user, $db_user);  // PDOインスタンスを生成
+                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // エラーモードの設定
+                        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // エミュレーションを停止。
+                 
                         $db->beginTransaction();
-                        $stmt = $db->prepare("INSERT INTO posts (post_category, post_content) VALUES (?, ?)");
-                        $stmt->execute(array($_POST["post_category"], $_POST["post_content"]));
+                        
+                        $sql = 'INSERT INTO posts (post_content) VALUES (:post_content)';
+                        $data = array(':post_content' => $_POST['post_content']);     //先にSQL文とexcecuteで使う配列データをそれぞれ変数にいれておく。
+                        
+                        $stmt = $db->prepare($sql);
+                        $stmt->execute($data);
+
                         $db->commit();
+
+                        // 以下４文はうまくいかなかった。
+                        // $db->beginTransaction();
+                        // $stmt = $db->prepare("INSERT INTO posts (post_content) VALUES (?)");
+                        // $stmt->execute(array($_POST["post_content"]));
+                        // $db->commit();
+
                     } catch (PDOException $e) {
-                    // エラー発生時
+                    // エラー発生時  
                     $db->rollBack();
                     exit("クエリの実行に失敗しました");
+                    // print_r($stmt -> errorInfo());     //エラー時確認用
                     }
 
                 // 完了
                 echo "登録されました。";
+                // var_dump($_POST["post_content"]);     //エラー時確認用
                 }
             ?>
 
             
-            <p>
-                <?php echo he($page_message); ?>     <!-- 登録されたら表示 -->
-            </p>
-            <p class="attention">
-                <?php echo he($page_error); ?>     <!-- エラーあるなら表示 -->
-            </p>
 
 
             <!-- 
@@ -92,45 +112,35 @@
                 なお、主なmethodには「post」と「get」があります。こういったフォーム送信については、まずは「post」で行うと覚えておいてください。
             -->
             <form action="post_edit.php" method="post">
-                <!-- 
-                    inputタグとtype=”text”の指定により、テキスト入力フォームが表示されます。
-                    name=”post_category”という指定により、入力された値はpost_categoryという名前で送信されます。
-                -->
-                <div>
-                ジャンル<br>
-                <input type="text" name="post_category" size="30" value="<?php echo he($request["post_category"]); ?>">
-                </div>
+                
                 <!--
                     textareaタグは、複数行のテキストを入力するための入力項目を表示します。
                     name=”post_content”という指定により、入力された値はpost_contentという名前で送信されます。
                     rows=”5″は入力枠の行数が5行であることを意味します。
-                    cols=”20″は入力枠の幅が20文字であることを意味します。
+                    cols=”20″は入力枠の幅が20文字であることを意味します。　　　
+                    rowsとcols属性ではなく、大きさはCSSでwidthとheightで指定するほうがブラウザの影響出なくて良し。
+                    placeholder属性でヒントを表示
                 -->
                 <div>
-                名言<br>
-                <textarea name="post_content" rows="10" cols="50"><?php echo he($request["post_content"]); ?></textarea>
+                <br>
+                <textarea name="post_content" placeholder="入力して登録ボタンを押してください。" ><?php echo he($_POST["post_content"]); ?></textarea>
                 </div>
+
                 <!--
                     type=”submit”と指定されたボタンをクリックされると、formの中身を送信します。
                     name=”send”という指定により、value値（「登録する」という文字列）はsendという名前で送信されます。
                     form の method 属性が「post」なので送信されたデータは、$_POST という連想配列（スーパーグローバル変数）に格納されます。送信ボタンの name 属性が「send」なのでスーパーグローバル変数 $_POST のキーに「send」を指定して $_POST['send'] とします。
                 -->
                 <div>
-                <input type="submit" name="send" value="登録する">
+                <input type="submit" name="send" value="登録" class="submit_btn">
                 </div>
             </form>
 
             <hr>
-            <a href="index.php">トップへ戻る</a>
+            <a href="index.php" class="a">トップへ戻る</a>
 
         </div>
-
-        <a href="index.php">トップへ戻る</a>
+        
 
   </body>
 </html>
-
-    
-    
-
-  
